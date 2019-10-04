4ba83f09020c16273a791336a57e63f7e17c5988
r-s-3 <45521666+r-s-3@users.noreply.github.com>
2019-10-04 12:44:18 +0200

    rename to Game.java, implement movePlayer method

---
+++ Game.java
@@ -0,0 +1,18 @@
+public class Game {
+
+    protected void movePlayer(int numberOfSquares){
+    /* moves player a number of square on the board
+       remove player from Queue
+       Player.moveForward(numberOfSquares);
+       
+    */
+    }
+
+    protected void findSquare(int squareNumber){
+    // returns the square on which the player is on
+    }
+
+    public static void main(String[] args) {
+    }
+
+}
