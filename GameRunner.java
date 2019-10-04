0fa59113f634dded07a58bc49037bfaf3db0e82c
r-s-3 <roman.schmid3@uzh.ch>
2019-10-04 15:35:31 +0200

    bug fixes

--- GameRunner.java
+++ GameRunner.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class GameRunner{
-    public void main(String[] args){
+    public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
 
@@ -12,10 +12,10 @@ public class GameRunner{
         int number_of_players = input.nextInt(); // must be between 2 and 4
 
         //ask for the names of the players:
-        System.out.print("State the names of the players: ");
+        System.out.print("State the names of the players: \n");
         List playerNames = new LinkedList();
         for (int i = 1; i <= number_of_players; i++){
-            System.out.print("Player" + i);
+            System.out.print("Player " + i + "\n");
             String name = input.next();
             playerNames.add(name);
         }
