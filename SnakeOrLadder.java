3793f50c210ef6bfae1faba9882238b333ca78b6
Florin Ulrich <florin.a.ulrich@bluewin.ch>
2019-10-05 13:55:45 +0200

    Added Square or ladder

---
+++ SnakeOrLadder.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+
+public class SnakeOrLadder implements ISquare {
+
+    //Variables
+    private int position;
+    private boolean isLastSquare;
+    private SquareType squareType;
+
+    private ISquare finalSquare;
+
+    private enum SquareType {
+        LADDER,
+        SNAKE
+    }
+
+    //Methods
+    @Override
+    public void leave(Player playerName) {
+        finalSquare.leave(playerName);
+
+    }
+
+    @Override
+    public ISquare moveAndLand(int numberOfSteps) {
+        return finalSquare.moveAndLand(numberOfSteps);
+    }
+
+    @Override
+    public ISquare landHereOrGoHome() {
+        return finalSquare.landHereOrGoHome();
+    }
+
+    @Override
+    public void enter(Player playerName) {
+        finalSquare.enter(playerName);
+    }
+
+    @Override
+    public boolean isLastSquare() {
+        return finalSquare.isLastSquare();
+    }
+
+    @Override
+    public void setAsLastSquare() {
+        finalSquare.setAsLastSquare();
+    }
+
+    @Override
+    public int getPosition() {
+        return finalSquare.getPosition();
+    }
+
+    @Override
+    public ArrayList<Player> getPlayerOnSquare() {
+        return finalSquare.getPlayerOnSquare();
+    }
+
+    @Override
+    public void addPlayer(Player player) {
+        finalSquare.addPlayer(player);
+    }
+}
