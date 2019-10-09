233944e38bbcf9b00cd04e083e5cfc52fbcb5cfd
Florin Ulrich <florin.a.ulrich@bluewin.ch>
2019-10-09 08:42:30 +0200

    First Build

--- Assignment2/src/HelloWorld.java
+++ Assignment2/src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("This is where Assignment 2 lives 0)");
+        System.out.println("This is where Assignment 2 lives =)");
     }
 }
