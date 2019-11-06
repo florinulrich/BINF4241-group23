c49974762d8b2b70234c6a6e9fb45c300e088b93
r-s-3 <roman.schmid3@uzh.ch>
2019-11-06 09:49:50 +0100

    add Singleton for CheckMate Class

--- Assignment3/src/Classes/Observers/CheckMate.java
+++ Assignment3/src/Classes/Observers/CheckMate.java
@@ -5,6 +5,14 @@ import Interfaces.CheckMateObserverInterfaces.CheckMateObserver;
 
 public class CheckMate implements CheckMateObserver {
 
+    private static  CheckMate uniqueInstance = new CheckMate();
+
+    private CheckMate(){}
+
+    public static CheckMate getInstance(){
+        return uniqueInstance;
+    }
+
     private boolean isCheckmate = false;
 
     @Override
