3843fce466a061ac93412f02c140baed4553631f
Florin Ulrich <florin.a.ulrich@bluewin.ch>
2019-11-13 10:25:40 +0100

    Implement timer

--- Assignment4/src/Main.java
+++ Assignment4/src/Main.java
@@ -2,11 +2,18 @@ public class Main {
 
     public static void main(String[] args){
 
-        Smartphone sp = new Smartphone();
-        Submenu testSubmenue = new Submenu("est");
+//        Smartphone sp = new Smartphone();
+//        Submenu testSubmenue = new Submenu("est");
+//
+//        sp.addSubmenu(testSubmenue);
+//        sp.display();
 
-        sp.addSubmenu(testSubmenue);
-        sp.display();
+        Timer timer = new Timer(5);
+        System.out.println("Set Timer");
+        timer.startTimer();
+        System.out.println("I do other stuff now");
+
+
 
     }
 }
--- Assignment4/src/Timer.java
+++ Assignment4/src/Timer.java
@@ -1,5 +1,42 @@
-public class Timer {
+public class Timer implements Runnable{
 
+    //Variables
+    private int remainingMinutes;
+    private boolean timerIsRunning = false;
+    private Thread timerThread = new Thread(this);
 
+    //Constructor
+    public Timer(int minutes) {
+        remainingMinutes = minutes;
+    }
 
+    //Methods
+    @Override
+    public void run() {
+        timerIsRunning = true;
+        while (remainingMinutes > 0) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            remainingMinutes--;
+        }
+
+        timerIsRunning = false;
+
+    }
+
+    public int getRemainingMinutes() {
+        return remainingMinutes;
+    }
+
+    void startTimer() {
+        timerThread.start();
+
+    }
+
+    boolean isRunning() {
+        return timerIsRunning;
+    }
 }
