ddc705493d1c93532e70a30b61f8b92d54464c6b
r-s-3 <roman.schmid3@uzh.ch>
2019-11-30 15:56:17 +0100

    add GameRunnerTest skeleton class

---
+++ Assignment5/Snakes and Ladders/src/GameRunnerTest.java
@@ -0,0 +1,45 @@
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GameRunnerTest {
+
+    @Before public void setUp(){
+
+    }
+
+    /**
+     *
+     */
+    @Test public void TestGetSetupInputs(){
+
+    }
+
+    /**
+     *
+     */
+    @Test public void TestAskForPlayerNumber(){
+
+    }
+
+    /**
+     *
+     */
+    @Test public void TestSetUpGame(){
+
+    }
+
+    /**
+     *
+     */
+    @Test public void TestAddSnakesAndLadders(){
+
+    }
+
+    @After public void tearDown(){
+
+    }
+}
