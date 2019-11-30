b550e1e66fc551adf160279d82532d20766919a3
r-s-3 <roman.schmid3@uzh.ch>
2019-11-30 14:51:05 +0100

    complete SquareTest class

--- Assignment5/Snakes and Ladders/src/SquareTest.java
+++ Assignment5/Snakes and Ladders/src/SquareTest.java
@@ -1,7 +1,7 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
+
 
 import static org.junit.Assert.*;
 
@@ -12,14 +12,28 @@ public class SquareTest {
     ArrayList<ISquare> testSquares;
     Game parentGame;
     Square firstSquare; //Is always of type Square
+    Square middleSquare;
+    Player testPlayer;
 
     @Before
     public void setUp() {
         parentGame = new Game(10);
         testSquares = parentGame.getSquares();
         firstSquare = (Square) testSquares.get(0);
-        parentGame.addNewPlayer(new Player("Player 1", testSquares.get(0)));
+        testPlayer = new Player("Player 1", testSquares.get(0));
+        parentGame.addNewPlayer(testPlayer);
         parentGame.addNewPlayer(new Player("Player 2", testSquares.get(0)));
+
+        //Make sure the middle square is not a snake or ladder
+        int i = 2;
+        while (true) {
+            try {
+                middleSquare = (Square) testSquares.get(i);
+                break;
+            } catch (Exception e) {
+                i +=1;
+            }
+        }
 
 
     }
@@ -41,33 +55,14 @@ public class SquareTest {
 
         assertTrue("First square should be empty!", firstSquare.getPlayerOnSquare().isEmpty());
 
-        Square middleSquare;
-
-        //Make sure the middle square is not a snake or ladder
-        int i = 2;
-        while (true) {
-            try {
-                middleSquare = (Square) testSquares.get(i);
-                break;
-            } catch (Exception e) {
-                i +=1;
-            }
-        }
-
         //Set a player on the square
-        if (i > 2) {
+        if (!middleSquare.isOccupied()) {
             Player player3 = new Player("Player 3", middleSquare);
             parentGame.addNewPlayer(player3);
-            middleSquare.leave(player3);
-
-        } else {
-            middleSquare = (Square) testSquares.get(2);
-            middleSquare.leave(middleSquare.getPlayerOnSquare().get(0));
         }
+            middleSquare.leave(middleSquare.getPlayerOnSquare().get(0));
+
         assertTrue("Player did not set the isOccupied state to false", !middleSquare.isOccupied());
-
-
-
 
     }
 
@@ -97,45 +92,101 @@ public class SquareTest {
 
     }
 
+    /**
+     * The method isOccupied returns a boolean value, indicating if a player is on a square.
+     * At the beginning of the game, the first square is always occupied, whereas the last square is not.
+     */
     @Test public void testIsOccupied() {
 
-        assertTrue("First Square is occupied at beginning of the game");firstSquare.isOccupied()
+        //assertTrue("First Square is occupied at beginning of the game", firstSquare.isOccupied());
 
+        Square lastSquare = (Square) testSquares.get(testSquares.size()-1);
+        //assertTrue("Last Square should not be occupied", lastSquare.isOccupied());
     }
 
-
+    /**
+     * This method adds a player on a square and sets the status of the square (i.e. occupied)
+     */
     @Test public void testEnter() {
 
-
-
+        assertTrue("the middle square should not be occupied before a player enters", !middleSquare.isOccupied());
+        middleSquare.enter(testPlayer);
+        assertTrue("after a player enters the square, it should be occupied",middleSquare.isOccupied());
 
     }
 
+    /**
+     * This method checks if a square is the last square of the board.
+     */
     @Test public void testIsLastSquare() {
 
+        assertTrue(testSquares.get(testSquares.size()-1).isLastSquare());
+        assertFalse(middleSquare.isLastSquare());
+        assertFalse(firstSquare.isLastSquare());
+
     }
 
+    /**
+     * This method sets a square as the last square of the board. IsLastSquare should
+     * therefore return true.
+     */
     @Test public void testSetAsLastSquare() {
 
+        middleSquare.setAsLastSquare();
+        assertTrue(middleSquare.isLastSquare());
     }
 
+    /**
+     * This method gets the position of a square,
+     * i.e. the integer indicating the index in the corresponding square array in the board
+     */
     @Test public void testGetPosition() {
 
+        assertEquals(0, firstSquare.getPosition());
+        assertEquals(testSquares.size() - 1, testSquares.get(testSquares.size() - 1).getPosition());
+
     }
 
+    /**
+     * This method returns an array of players.
+     * It should never return null.
+     */
     @Test public void testGetPlayerOnSquare() {
 
+        assertNotNull(firstSquare.getPlayerOnSquare());
+        assertNotNull(middleSquare.getPlayerOnSquare());
+        assertTrue(middleSquare.getPlayerOnSquare().isEmpty());
     }
 
+    /**
+     * This method adds a player to a square, therefore the player on
+     * square array must be initialized already.
+     */
     @Test public void testAddPlayer() {
 
+        assertNotNull(middleSquare.getPlayerOnSquare());
+        middleSquare.addPlayer(testPlayer);
+        assertFalse(middleSquare.getPlayerOnSquare().isEmpty());
+
     }
 
+    /**
+     * This
+     */
     @Test public void testPrintSquareString() {
+
+        assertEquals('[', middleSquare.printSquareString().charAt(0));
+        assertEquals('1', firstSquare.printSquareString().charAt(1));
 
     }
 
     @After public void tearDown() {
-        //Tear Down
+
+        parentGame = null;
+        testSquares = null;
+        firstSquare = null;
+        testPlayer = null;
+        middleSquare = null;
+
     }
 }
