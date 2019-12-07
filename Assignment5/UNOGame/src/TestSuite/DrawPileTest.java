07537384443118a925a05c86ebd378d776ae951d
r-s-3 <roman.schmid3@uzh.ch>
2019-12-07 12:12:22 +0100

    add Game Test classes to TestSuite

---
+++ Assignment5/UNOGame/src/TestSuite/DrawPileTest.java
@@ -0,0 +1,30 @@
+package TestSuite;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DrawPileTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void shuffle() {
+    }
+
+    @Test
+    void draw() {
+    }
+
+    @Test
+    void getPlayedCardBack() {
+    }
+}
---
+++ Assignment5/UNOGame/src/TestSuite/PlayerQueueTest.java
@@ -0,0 +1,38 @@
+package TestSuite;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerQueueTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void skipOne() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void reverse() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void next() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setDealer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isEmpty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addPlayer() {
+    }
+}
