d0de4c854523483c3e20d7996df7ae693ea2ccbc
Florin Ulrich <florin.a.ulrich@bluewin.ch>
2019-12-07 12:12:32 +0100

    Add further test classes

---
+++ Assignment5/UNOGame/src/TestSuite/DiscardPileTest.java
@@ -0,0 +1,42 @@
+package TestSuite;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiscardPileTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void isLegalNextCard() {
+    }
+
+    @Test
+    void setLegalNextColor() {
+    }
+
+    @Test
+    void tellNextPlayerToDraw() {
+    }
+
+    @Test
+    void skipOnePlayer() {
+    }
+
+    @Test
+    void reversePlayerOrder() {
+    }
+
+    @Test
+    void getUsedCards() {
+    }
+}
---
+++ Assignment5/UNOGame/src/TestSuite/PlayerTest.java
@@ -0,0 +1,38 @@
+package TestSuite;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void yourTurn() {
+    }
+
+    @Test
+    void challengeUNO() {
+    }
+
+    @Test
+    void challengeWildCard() {
+    }
+
+    @Test
+    void takeCards() {
+    }
+
+    @Test
+    void chooseColor() {
+    }
+}
