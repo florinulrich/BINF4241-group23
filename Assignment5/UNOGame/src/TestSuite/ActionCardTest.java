f3378616ff0a30a68a485575fba2ad66e727de01
Florin Ulrich <florin.a.ulrich@bluewin.ch>
2019-12-07 12:09:41 +0100

    Set up first test classes

---
+++ Assignment5/UNOGame/src/TestSuite/ActionCardTest.java
@@ -0,0 +1,26 @@
+package TestSuite;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActionCardTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void executeAction() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getColor() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void hasActions() {
+    }
+}
---
+++ Assignment5/UNOGame/src/TestSuite/NumberCardTest.java
@@ -0,0 +1,30 @@
+package TestSuite;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberCardTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getNumber() {
+    }
+
+    @Test
+    void getColor() {
+    }
+
+    @Test
+    void hasActions() {
+    }
+}
